285ddb5913520d48c78b328f59ba730c8448244f
ibBenwah <garret.c.benoit@gmail.com>
2023-01-12 21:45:47 -0500

    redid pizza lab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'ChurpusPizzaLab';
-}
+  title: string = 'ChurpusPizzaLab';
+    
+  Menu: MenuItem[] = [
+    {
+      item: "chicken fingers",
+      category: "Starters",
+      price: 8.99
+    },
+    {
+      item: "Pizza",
+      category: "Dinner",
+      price: 5.99
+    },
+    {
+      item: "BreadSticks",
+      category: "Starters",
+      price: 2.99
+    },
+    {
+      item: "Wings",
+      category: "Dinner",
+      price: 9.99
+    },
+    {
+      item: "Cinnamon Roll",
+      category: "Dessert",
+      price: 5.49
+    },
+    {
+      item: "Pop",
+      category: "Drinks",
+      price: 1.99
+    },
+    {
+      item: "Beer",
+      category: "Drinks",
+      price: 4.99
+    }
+  ];
+  
+  getByCategory(cat:string): MenuItem[]{
+    let result: MenuItem[] = [];
+
+    this.Menu.forEach((i:MenuItem) => {
+      if(i.category == cat){
+        result.push(i);
+      }
+    });
+
+      return result;
+  }
+};
